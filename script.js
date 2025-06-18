10409d0dfe8a96686cb4f8c676bd44b0dbc400b1
Tonito <tonitosison@gmail.com>
2025-06-18 21:26:52 +0800

    Added Buttons and Operator Functions

---
+++ script.js
@@ -0,0 +1,36 @@
+let operation = {
+  number1: 0,
+  sign: '',
+  number2: 0
+}
+
+function add(first, second) {
+  return first + second;
+}
+
+function subtract(first, second) {
+  return first - second;
+}
+
+function multiply(first, second) {
+  return first * second;
+}
+
+function divide (first, second){
+ return first / second;
+}
+
+function operate (equation){
+  if (equation.sign === '+') {
+    return add(equation.number1, equation.number2);
+  }
+  else if (equation.sign === '-') {
+    return subtract(equation.number1, equation.number2);
+  }
+  else if (equation.sign === '*') {
+    return multiply(equation.number1, equation.number2);
+  }
+  else if (equation.sign === '/') {
+    return divide (equation.number1, equation.number2);
+  }
+}
