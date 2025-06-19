23fcf9ade09a3ec0f12c1060cc5cbeaf220693d6
Tonito <tonitosison@gmail.com>
2025-06-20 00:25:31 +0800

    Draft of Display

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+const buttons = document.getElementsByTagName("button");
+const buttontxt = buttons.innerText || buttons.TextContent;
+const output = document.getElementById("display");
+
+let display = "0";
+
 let operation = {
   number1: 0,
   sign: '',
   number2: 0
 }
 
-let display = 0;
+function change_display () {
+  buttons.addEventListener("mouseclick", () => {
+    output.innerHTML = display;
+  })
+}
+
+change_display();
 
 function add(first, second) {
   return first + second;
